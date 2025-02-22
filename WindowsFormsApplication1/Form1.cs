219a1273b9b6520c07b71de5903f85e9447e2930
minh khue <khue.ptm@ou.edu.vn>
2025-02-22 16:17:29 +0700

    add new line

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -30,6 +30,7 @@ namespace WindowsFormsApplication1
         private void button2_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Modified by Feature-cal");
+	    MessageBox.Show("Another line added by Feature-cal");
 
         }
     }
